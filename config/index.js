e773ce9b22178b2f1e9684a23dc3800cf5c42e49
krishnanpandya007 <21dce067@charusat.edu.in>
2022-06-24 15:46:13 +0530

    Addded new client

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ export const FRONTEND_ROOT_URL = 'https://king-prawn-app-3fykl.ondigitalocean.ap
 
 export const defaultBorderColor = '#bdbdbd'
 
-export const CLIENT_ID = 'A2AkRhbxT0wd3c0u3WaZuJ6UNRtJm5a3bcj4ycQx'
+export const CLIENT_ID = '5byku1c4U1Y6JnvfvYKUxnOrjxKTXVmnBmnuD6x2'
 
-export const CLIENT_SECRET = 'pbkdf2_sha256$320000$tCqIPz2TDTlqjUE2gjn4cm$CPZ86Z2b7RJT+Kjh1BlYNh1E6kArReYIjCFYnqCyAFg='
+export const CLIENT_SECRET = 'fsyPJ9HuikCSnMYvTc2OC7x7k7xaNKGli5L4uFRiCZnqM3bin21EyEIiAhCrp57G8val5Of0za2XKYB8R7XFKJohyn1X3QaSJDfWVOkTg2dObbZAL73nbdU82Uo6oqR5'
 
