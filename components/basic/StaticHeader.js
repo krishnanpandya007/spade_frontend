cd19f85f92cbce6fdd3da29d8145f136ef25cf04
krishnanpandya007 <21dce067@charusat.edu.in>
2022-08-11 17:51:42 +0530

    Fixed SocialError Page 2

--- components/basic/StaticHeader.js
+++ components/basic/StaticHeader.js
@@ -6,7 +6,7 @@ import { FRONTEND_ROOT_URL } from '../../config'
 function StaticHeader() {
   return (
     <center>
-        <header style={{width: 'clamp(400px, 80vw, 1200px)', padding: '1.3rem', display: 'flex', justifyContent: 'space-between'}}>
+        <header style={{width: 'clamp(300px, 80vw, 1200px)', padding: '1.3rem', display: 'flex', justifyContent: 'space-between'}}>
             <Link href="/">
               <a href="/">
                 <Image src="/spade_icon.svg" width="25" height="25" />
