b1e0c9beb6f20a3c4a2f3ffe0bf971413d2417eb
krishnanpandya007 <21dce067@charusat.edu.in>
2022-08-11 17:12:45 +0530

    Fixed SocialError Page

--- pages/social_account/error.js
+++ pages/social_account/error.js
@@ -12,12 +12,12 @@ export default function SocialConnectError() {
       <h2>Error Occured while Syncing info.</h2>
       <br/>
       {
-        router.query?.error ? <b>Error Message:</b><span>{router.query.error}</span>
+        router.query?.error ? <b>Error Message:<span>{router.query.error}</span></b>:null
       }
       <br/>
       <br/>
 
-      <p>You can also create account<span><Link href="create/account"><a href="create/account"> natively.</a></Link></span>
+      <p>You can also create account<span><Link href="create/account"><a href="create/account"> natively.</a></Link></span></p>
 
 
     </center>
