b839a51026637b6b51a91f700ed907a7f758a899
krishnanpandya007 <21dce067@charusat.edu.in>
2022-09-23 16:57:21 +0530

    fixed: comfrim -> confirm ;2

--- components/DailyShare.js
+++ components/DailyShare.js
@@ -809,7 +809,7 @@ function AudioPost({ data }) {
 
   const deletePost = async () => {
 
-    let confirmation = comfirm('Are you sure, delete this post Permanently?')
+    let confirmation = confirm('Are you sure, delete this post Permanently?')
 
     if(!confirmation){
       return;
@@ -1111,7 +1111,7 @@ function TextPost({ data }) {
 
   const deletePost = async () => {
 
-    let confirmation = comfirm('Are you sure, delete this post Permanently?')
+    let confirmation = confirm('Are you sure, delete this post Permanently?')
 
     if(!confirmation){
       return;
@@ -1275,7 +1275,7 @@ function TextPost({ data }) {
         <h2 style={{padding: '1rem 0 0 1rem', marginBlock: '0', color: '#181818'}}>{data.title}</h2>
         <div onMouseEnter={(e) => {e.target.style.cursor = 'pointer'}} style={{position: 'absolute', right: '0.7rem', top: '0.2rem'}}>
 
-        <svg onClick={(e) => { setShowMoreOptions(curr => !curr) }} width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg {...handlers} onClick={(e) => { setShowMoreOptions(curr => !curr) }} width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M3.625 7.5C3.625 8.12132 3.12132 8.625 2.5 8.625C1.87868 8.625 1.375 8.12132 1.375 7.5C1.375 6.87868 1.87868 6.375 2.5 6.375C3.12132 6.375 3.625 6.87868 3.625 7.5ZM8.625 7.5C8.625 8.12132 8.12132 8.625 7.5 8.625C6.87868 8.625 6.375 8.12132 6.375 7.5C6.375 6.87868 6.87868 6.375 7.5 6.375C8.12132 6.375 8.625 6.87868 8.625 7.5ZM12.5 8.625C13.1213 8.625 13.625 8.12132 13.625 7.5C13.625 6.87868 13.1213 6.375 12.5 6.375C11.8787 6.375 11.375 6.87868 11.375 7.5C11.375 8.12132 11.8787 8.625 12.5 8.625Z" fill="#6BA37E"/>
         </svg>
 
