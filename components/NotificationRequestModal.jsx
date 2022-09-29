aa78f0d5250422024b08a91a2e023922d38163fb
krishnanpandya007 <21dce067@charusat.edu.in>
2022-09-29 18:47:23 +0530

    fixed: Wrong configs on Notification service on client_side

--- components/NotificationRequestModal.jsx
+++ components/NotificationRequestModal.jsx
@@ -5,6 +5,23 @@ import Lottie from 'lottie-web'
 import {LoadingButton} from '@mui/lab'
 import { FRONTEND_ROOT_URL } from "../config/index"
 
+// Web-Push
+// Public base64 to Uint
+function urlBase64ToUint8Array(base64String) {
+  var padding = '='.repeat((4 - base64String.length % 4) % 4);
+  var base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+  var rawData = window.atob(base64);
+  var outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 function Body({ notification_success }) {
 
   return (
@@ -59,7 +76,7 @@ function Header() {
   return (
 
     <div style={{ borderRadius: '10px 10px 0 0', backgroundColor: '#3B44F6', width: '100%', fontFamily: 'Poppins', position: 'relative', marginBottom: '2rem'}}>
-      <center><h2 style={{marginBlock: '0', padding: '1.2rem 0', fontSize: '1.3rem', fontWeight: '700', border: '1px solid red'}}>Lemme Notify!</h2></center>
+      <h2 style={{marginBlock: '0', padding: '1.2rem 0', color: 'white', fontSize: '1.3rem', fontWeight: '700', width: '85%', marginRight: '0', marginLeft: 'auto', textAlign: 'center'}}>Lemme Notify!</h2>
       <div ref={notificationAnimationRef} style={{position: 'absolute', left: '1.5rem', top: '2rem', borderRadius: '15px', width: '60px', height: '60px', backgroundColor: 'white',filter: 'drop-shadow(0px 0px 4px rgba(0, 0, 0, 0.25))'}}>
 
       </div>
