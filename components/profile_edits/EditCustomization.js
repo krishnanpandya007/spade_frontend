caa613c6abccb2e5c79f741b7c4b9b816d47ce54
krishnanpandya007 <21dce067@charusat.edu.in>
2022-10-22 01:49:59 +0530

    added: notification manager to settings of profile

--- components/profile_edits/EditCustomization.js
+++ components/profile_edits/EditCustomization.js
@@ -6,6 +6,7 @@ import { blue, red } from '@mui/material/colors'
 import styled from '@emotion/styled'
 import EditShareReccomendation from '../EditShareReccomendation'
 import authContext from '../basic/contexts/layout_auth_context'
+import NotificationRequestModal from '../NotificationRequestModal'
 // Remaining to show pre-first_name and pre-last_name
 
 const CenteredDiv = styled.div`
@@ -25,13 +26,14 @@ const CenteredDiv = styled.div`
 export default function EditCustomization({styl}) {
 
     const [openChangeRecommendation, setOpenChangeRecommendation] = React.useState(false)
+    const [notificationModalOpen,setNotificationModalOpen] = React.useState(false);
 
     const auth = useContext(authContext)
 
   return (
     <div style={{ ...styl,width: auth.is_on_mobile ? '100%' : '40%', padding: '0 3%', position: 'relative'}}>
 
-
+    {notificationModalOpen && <NotificationRequestModal />}
 
     {!auth.is_on_mobile && <h3>Edit Recommendations:</h3>}
     <p>You can edit/change your recommendations for your account to other account&apos;s recommendations!</p> 
@@ -46,6 +48,9 @@ export default function EditCustomization({styl}) {
         openChangeRecommendation ? <EditShareReccomendation isOpen={openChangeRecommendation} _onClose={() => {setOpenChangeRecommendation(false)}} />:
 <Button variant="contained" style={{position: 'absolute', bottom: auth.is_on_mobile ? '-3rem':'-1.2rem', right: '1rem', borderRadius :'100px'}} onClick={() => {setOpenChangeRecommendation(true) }} >Change Recommendations</Button>
     }
+    <br/>
+    <br/>
+    <p onClick={() => {setNotificationModalOpen(true)}} style={{textDecoration: 'underline', color: 'Background'}}>Manage Notifications</p>
 </div>
   )
 }
